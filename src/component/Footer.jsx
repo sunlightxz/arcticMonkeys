096bec15aa40829349b8712051b2b4786b577284
Amine As <amineasfar101@gmail.com>
2024-05-17 20:33:33 +0100

    chang

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='py-4 flex justify-center items-center'>
-      <h1>Made by Sunlightxz</h1>
+      <h1>Made by Sunlightxz.</h1>
     </div>
   )
 }
